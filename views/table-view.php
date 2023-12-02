9472bc236b1628ed1bf427fcc12932b07f857b7a
MalekAldaas <malek.a.aldaas@gmail.com>
2023-12-02 23:21:53 +0300

    modified table-view.php, added style.css to style the table.

--- views/table-view.php
+++ views/table-view.php
@@ -1,45 +1,59 @@
 <?php
 
-    require_once __DIR__ . "/../core/database.php"; 
-    $db = new Database(); 
-    $data = $db->select('categories') ;
+require_once __DIR__ . "/../core/database.php";
+$db = new Database();
+$data = $db->select('categories');
+
+
+$cssFilePath = __DIR__ . "/style.css";
+if (file_exists($cssFilePath)) {
+    echo "CSS file found: $cssFilePath";
+} else {
+    echo "CSS file not found: $cssFilePath";
+}
+
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    
+    <link rel="stylesheet" href="../../views/style.css"> 
+    <!--
+        how to edit the above href so that no problem appears
+        if I called the table-view.html anywhere else other than
+        categories directory?.
+    -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>table view</title>
 </head>
+
+<body>
     <table>
         <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Created at</th>
         </tr>
-        <?php foreach($data as $item): ?>
+        <?php foreach ($data as $item): ?>
             <tr>
-                <td><?= $item['id'] ?></td>
-                <td><?= $item['name'] ?></td>
-                <td><?= $item['created_at'] ?></td>
+                <td>
+                    <?= $item['id'] ?>
+                </td>
+                <td>
+                    <?= $item['name'] ?>
+                </td>
+                <td>
+                    <?= $item['created_at'] ?>
+                </td>
             </tr>
-            <?php endforeach ?>
+        <?php endforeach ?>
     </table>
-<body>
-    
+
 </body>
+
 </html>
-
-<style>
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
-
-    th, td {
-        border: 1px solid black;
-        padding: 8px;
-        text-align: center;
-    }
-</style>
